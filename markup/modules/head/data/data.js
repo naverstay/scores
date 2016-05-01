8167abb81ca434053d696b2eac8f6aa93603acfe
extonec (Roman A) <extonec@gmail.com>
2016-05-01 18:31:40 +0300

    git init my-tars
    Signed-off-by: extonec (Roman A) <extonec@gmail.com>

---
+++ markup/modules/head/data/data.js
@@ -0,0 +1,11 @@
+data = {
+    head: {
+        defaults: {
+            title: 'default title',
+            useSocialMetaTags: false,
+            favicons: 'short',
+            showSpecials: false,
+            ie9: false
+        }
+    }
+};
